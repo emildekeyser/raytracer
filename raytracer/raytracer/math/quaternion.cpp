ec3c429ab820090148492611df5aa65155900701
emildekeyser <emil.dekeyser@student.ucll.be>
2020-10-06 17:58:10 +0200

    romano's non compiling (disfunctional?) quaternions

--- raytracer/raytracer/math/quaternion.cpp
+++ raytracer/raytracer/math/quaternion.cpp
@@ -1,77 +1,77 @@
-#include "math/quaternion.h"
+/* #include "math/quaternion.h" */
 
-using namespace math;
-
-
-Point3D math::Quaternion::rotate(const Point3D& p)
-{
-	auto p_quar = Quaternion(0, p.x, p.y, p.z);
-	auto q_quar = *this;
-
-	auto p_quar_rot = q_quar * p_quar * q_quar.conjugate();
-
-	return Point3D(p_quar_rot.b, p_quar.c, p_quar_rot.d);
-}
-
-Quaternion math::Quaternion::conjugate()
-{
-	return Quaternion(this->a, -this->b, -this->c, -this->d);
-}
+/* using namespace math; */
 
 
-Quaternion math::operator +(const Quaternion& q1, const Quaternion& q2)
-{
-	return Quaternion(q1.a + q2.a, 
-					  q1.b + q2.b, 
-					  q1.c + q2.c, 
-					  q1.d + q2.d);
-}
+/* Point3D math::Quaternion::rotate(const Point3D& p) */
+/* { */
+/* 	auto p_quar = Quaternion(0, p.x, p.y, p.z); */
+/* 	auto q_quar = *this; */
 
-Quaternion math::operator -(const Quaternion& q1, const Quaternion& q2)
-{
-	return Quaternion(q1.a - q2.a, 
-					  q1.b - q2.b, 
-					  q1.c - q2.c, 
-					  q1.d - q2.d)
-}
+/* 	auto p_quar_rot = q_quar * p_quar * q_quar.conjugate(); */
 
-Quaternion math::operator *(const Quaternion& q1, const Quaternion& q2)
-{
-	return Quaternion(q1.a * q2.a - q1.b * q2.b - q1.c * q2.c - q1.d * q2.d,
-					  q1.a * q2.b + q1.b * q2.a + q1.c * q2.d - q1.d * q2.c,
-					  q1.a * q2.c - q1.b * q2.d + q1.c * q2.a + q1.d * q2.b,
-					  q1.a * q2.d + q1.b * q2.c - q1.c * q2.b + q1.d * q2.a);
-}
+/* 	return Point3D(p_quar_rot.b, p_quar.c, p_quar_rot.d); */
+/* } */
 
-Quaternion math::operator *(const Quaternion& q, double x)
-{
-	return Quaternion(q.a * x, q.b * x, q.c * x, q.d * x);
-}
-
-Quaternion math::operator *(double x, const Quaternion& q)
-{
-	return x * q;
-}
+/* Quaternion math::Quaternion::conjugate() */
+/* { */
+/* 	return Quaternion(this->a, -this->b, -this->c, -this->d); */
+/* } */
 
 
-Quaternion math::operator /(const Quaternion& q, double x)
-{
-	return q * (1 / x);
-}
+/* Quaternion math::operator +(const Quaternion& q1, const Quaternion& q2) */
+/* { */
+/* 	return Quaternion(q1.a + q2.a, */ 
+/* 					  q1.b + q2.b, */ 
+/* 					  q1.c + q2.c, */ 
+/* 					  q1.d + q2.d); */
+/* } */
 
-Quaternion math::operator +=(const Quaternion& q1, const Quaternion& q2)
-{
-	return q1 + q2;
-}
-Quaternion math::operator -=(const Quaternion& q1, const Quaternion& q2)
-{
-	return q1 - q2;
-}
-Quaternion math::operator *=(const Quaternion& q1, double x)
-{
-	return q1 * x;
-}
-Quaternion math::operator /=(const Quaternion& q1, double x)
-{
-	return q1 * (1 /x);
-}
+/* Quaternion math::operator -(const Quaternion& q1, const Quaternion& q2) */
+/* { */
+/* 	return Quaternion(q1.a - q2.a, */ 
+/* 					  q1.b - q2.b, */ 
+/* 					  q1.c - q2.c, */ 
+/* 					  q1.d - q2.d) */
+/* } */
+
+/* Quaternion math::operator *(const Quaternion& q1, const Quaternion& q2) */
+/* { */
+/* 	return Quaternion(q1.a * q2.a - q1.b * q2.b - q1.c * q2.c - q1.d * q2.d, */
+/* 					  q1.a * q2.b + q1.b * q2.a + q1.c * q2.d - q1.d * q2.c, */
+/* 					  q1.a * q2.c - q1.b * q2.d + q1.c * q2.a + q1.d * q2.b, */
+/* 					  q1.a * q2.d + q1.b * q2.c - q1.c * q2.b + q1.d * q2.a); */
+/* } */
+
+/* Quaternion math::operator *(const Quaternion& q, double x) */
+/* { */
+/* 	return Quaternion(q.a * x, q.b * x, q.c * x, q.d * x); */
+/* } */
+
+/* Quaternion math::operator *(double x, const Quaternion& q) */
+/* { */
+/* 	return x * q; */
+/* } */
+
+
+/* Quaternion math::operator /(const Quaternion& q, double x) */
+/* { */
+/* 	return q * (1 / x); */
+/* } */
+
+/* Quaternion math::operator +=(const Quaternion& q1, const Quaternion& q2) */
+/* { */
+/* 	return q1 + q2; */
+/* } */
+/* Quaternion math::operator -=(const Quaternion& q1, const Quaternion& q2) */
+/* { */
+/* 	return q1 - q2; */
+/* } */
+/* Quaternion math::operator *=(const Quaternion& q1, double x) */
+/* { */
+/* 	return q1 * x; */
+/* } */
+/* Quaternion math::operator /=(const Quaternion& q1, double x) */
+/* { */
+/* 	return q1 * (1 /x); */
+/* } */
--- raytracer/raytracer/math/quaternion.h
+++ raytracer/raytracer/math/quaternion.h
@@ -1,46 +1,46 @@
-#pragma once
+/* #pragma once */
 
-#include "math/angle.h"
-#include "math/vector.h"
-#include "math/point.h"
+/* #include "math/angle.h" */
+/* #include "math/vector.h" */
+/* #include "math/point.h" */
 
-namespace math
-{
-	struct Quaternion
-	{
-		double a;
-		double b;
-		double c;
-		double d;
+/* namespace math */
+/* { */
+/* 	struct Quaternion */
+/* 	{ */
+/* 		double a; */
+/* 		double b; */
+/* 		double c; */
+/* 		double d; */
 
-	public:
-		Quaternion(double a, double b, double c, double d) 
-			: a(a), b(b), c(c), d(d) {};
+/* 	public: */
+/* 		Quaternion(double a, double b, double c, double d) */ 
+/* 			: a(a), b(b), c(c), d(d) {}; */
 
-		static Quaternion rotation(Angle theta, const Vector3D& axis)
-		{
-			auto a = cos(theta / 2);
-			auto b = sin(theta / 2) * axis.x;
-			auto c = sin(theta / 2) * axis.y;
-			auto d = sin(theta / 2) * axis.z;
+/* 		static Quaternion rotation(Angle theta, const Vector3D& axis) */
+/* 		{ */
+/* 			auto a = cos(theta / 2); */
+/* 			auto b = sin(theta / 2) * axis.x; */
+/* 			auto c = sin(theta / 2) * axis.y; */
+/* 			auto d = sin(theta / 2) * axis.z; */
 
-			return Quaternion(a, b, c, d);
-		}
+/* 			return Quaternion(a, b, c, d); */
+/* 		} */
 
-		Point3D rotate(const Point3D& p);
-	protected:
-		Quaternion conjugate();
-	};
+/* 		Point3D rotate(const Point3D& p); */
+/* 	protected: */
+/* 		Quaternion conjugate(); */
+/* 	}; */
 
-	Quaternion operator +(const Quaternion&, const Quaternion&);
-	Quaternion operator -(const Quaternion&, const Quaternion&);
-	Quaternion operator *(const Quaternion&, const Quaternion&);
-	Quaternion operator *(double, const Quaternion&);
-	Quaternion operator *(const Quaternion&, double);
-	Quaternion operator /(const Quaternion&, double);
+/* 	Quaternion operator +(const Quaternion&, const Quaternion&); */
+/* 	Quaternion operator -(const Quaternion&, const Quaternion&); */
+/* 	Quaternion operator *(const Quaternion&, const Quaternion&); */
+/* 	Quaternion operator *(double, const Quaternion&); */
+/* 	Quaternion operator *(const Quaternion&, double); */
+/* 	Quaternion operator /(const Quaternion&, double); */
 
-	Quaternion operator +=(const Quaternion&, const Quaternion&);
-	Quaternion operator -=(const Quaternion&, const Quaternion&);
-	Quaternion operator *=(const Quaternion&, double);
-	Quaternion operator /=(const Quaternion&, double);
-}
+/* 	Quaternion operator +=(const Quaternion&, const Quaternion&); */
+/* 	Quaternion operator -=(const Quaternion&, const Quaternion&); */
+/* 	Quaternion operator *=(const Quaternion&, double); */
+/* 	Quaternion operator /=(const Quaternion&, double); */
+/* } */
